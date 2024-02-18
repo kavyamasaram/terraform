1f6b3ad78c0e42034d5d69fb99154655366ef624
root <root@ip-172-31-8-104.ap-south-1.compute.internal>
2024-02-18 09:24:55 +0000

    code

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "~>4.0"
+    }
+    google = {
+    }
+    azurerm = {
+      version = "3.0.0"
+    }
+  }
+}
+
+
+
+
+
+
+resource "local_file" "resource-one" {
+  filename = "a.txt"
+  content  = "foo!"
+}
+
+#resource "local_file" "resource-two"  {
+
+#}
+
